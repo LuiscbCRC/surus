a7b9869b865e76e3a151ed9f422174685257abcf
Luis Antonio Castillo Benavides <luiscb04@gmail.com>
2022-10-10 22:03:42 -0600

    Custom fixes for VIEW

--- lib/surus/json/belongs_to_scope_builder.rb
+++ lib/surus/json/belongs_to_scope_builder.rb
@@ -4,18 +4,27 @@ module Surus
       def scope
         s = association
           .klass
-          .where("#{association_primary_key}=#{association_foreign_key}")
+          .where("#{primary_table}.#{primary_key}=#{association_table}.#{association_foreign_key}")
         s = s.instance_eval(&association.scope) if association.scope
         s
       end
 
-      def association_primary_key
-        "#{quote_table_name association.table_name}.#{quote_column_name association.active_record_primary_key}"
+      def primary_table
+        connection.quote_table_name association.klass.table_name
+      end
+
+      def primary_key
+        quote_column_name association.active_record_primary_key
+      end
+
+      def association_table
+        outside_class.quoted_table_name
       end
 
       def association_foreign_key
-        "#{quote_table_name outside_class.table_name}.#{quote_column_name association.foreign_key}"
+        quote_column_name association.foreign_key
       end
+
     end
   end
 end
